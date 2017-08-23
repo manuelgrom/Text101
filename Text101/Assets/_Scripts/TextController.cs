b82c45213a1635c3876ce88d0c6c1cc7fef38e33
Manuel <manuelgrom@users.noreply.github.com>
2017-08-23 13:36:35 +0200

    UI Text and Image

---
+++ Text101/Assets/_Scripts/TextController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+        text.text = "Hello world!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            text.text = "space presse";
+        }
+    }
+}
